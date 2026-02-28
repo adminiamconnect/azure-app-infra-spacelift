fe9fecfd5bf9476174807983aa74ef7d15b71538
michaelcorrodus <admin@iamconnect.co.uk>
2026-02-28 07:03:53 +0000

    Remove web block from Salesforce application config
    Removed web block and its redirect URIs from the Salesforce application configuration.

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,13 +7,6 @@ resource "azuread_application" "salesforce_app" {
   display_name = "Salesforce Global"
 }
 
-  web {
-    redirect_uris = [
-      "https://login.salesforce.com",
-      "https://mydomain.my.salesforce.com"
-    ]
-  }
-
 resource "azuread_service_principal" "salesforce_sp" {
   application_id = azuread_application.salesforce_app.application_id
 }
