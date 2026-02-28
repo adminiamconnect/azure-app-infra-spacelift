f971654cdd4165820d8fceda6dfbd5efbd2434a8
michaelcorrodus <admin@iamconnect.co.uk>
2026-02-28 06:50:16 +0000

    Update main.tf

--- modules/app-service/main.tf
+++ modules/app-service/main.tf
@@ -1,26 +1,3 @@
 variable "app_name" {}
 variable "location" {}
 variable "resource_group_name" {}
-
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-resource "azurerm_app_service_plan" "plan" {
-  name                = "${var.app_name}-plan"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_app_service" "app" {
-  name                = var.app_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
-}
