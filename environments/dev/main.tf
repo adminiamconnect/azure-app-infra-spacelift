d425ebc1928260f7b9a2cd648b9f7bc2e4d1c93b
michaelcorrodus <admin@iamconnect.co.uk>
2026-02-28 06:52:32 +0000

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,6 @@
-module "app_service" {
-  source              = "../../modules/app-service"
-  app_name            = "dev-myapp"
-  location            = "UK South"
-  resource_group_name = "rg-dev-myapp"
-}
+# module "app_service" {
+#   source              = "../../modules/app-service"
+#   app_name            = "dev-myapp"
+#   location            = "UK South"
+#   resource_group_name = "rg-dev-myapp"
+# }
