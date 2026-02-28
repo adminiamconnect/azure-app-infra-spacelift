6586971af24329fc0d417d0e31a13c2d7b027a56
michaelcorrodus <admin@iamconnect.co.uk>
2026-02-28 07:01:42 +0000

    Add Salesforce Azure AD resources to main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,6 +1,18 @@
-# module "app_service" {
-#   source              = "../../modules/app-service"
-#   app_name            = "dev-myapp"
-#   location            = "UK South"
-#   resource_group_name = "rg-dev-myapp"
-# }
+resource "azuread_group" "salesforce_users" {
+  display_name     = "Salesforce Global Users"
+  security_enabled = true
+}
+
+resource "azuread_application" "salesforce_app" {
+  display_name = "Salesforce Global"
+
+  web {
+    redirect_uris = [
+      "https://login.salesforce.com",
+      "https://mydomain.my.salesforce.com"
+    ]
+  }
+
+resource "azuread_service_principal" "salesforce_sp" {
+  application_id = azuread_application.salesforce_app.application_id
+}
