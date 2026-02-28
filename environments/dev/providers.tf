f021752bbf697710b09f910aaf4e590a3d7143d6
michaelcorrodus <admin@iamconnect.co.uk>
2026-02-28 05:02:13 +0000

    Update Azure provider version and OIDC setting

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
   features {}
-  use_oidc = true
 }
 
 terraform {
@@ -9,7 +8,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.90"
     }
   }
 }
