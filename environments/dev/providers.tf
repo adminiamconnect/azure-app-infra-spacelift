237a5ac29a411b4b1ffa18047ec12e04097373ee
michaelcorrodus <admin@iamconnect.co.uk>
2026-02-28 05:47:29 +0000

    Add Azure AD provider and required version constraints

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -1,3 +1,20 @@
 provider "azurerm" {
   features {}
 }
+
+provider "azuread" {}
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
+  }
+}
