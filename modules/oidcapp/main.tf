acb6ddb221568c3b7e89c3c8f0a3298669b1640d
michaelcorrodus <admin@iamconnect.co.uk>
2026-03-03 06:34:36 +0000

    Refactor OIDC app variables and resources

--- modules/oidcapp/main.tf
+++ modules/oidcapp/main.tf
@@ -1,11 +1,16 @@
-variable "app_name" {
-  type = string
+variable "app_name" {}
+variable "redirect_uris" {
+  type = list(string)
 }
 
-resource "azuread_application" "app" {
+resource "azuread_application" "this" {
   display_name = var.app_name
+
+  web {
+    redirect_uris = var.redirect_uris
+  }
 }
 
-resource "azuread_service_principal" "sp" {
-  client_id = azuread_application.app.client_id
+resource "azuread_service_principal" "this" {
+  client_id = azuread_application.this.client_id
 }
